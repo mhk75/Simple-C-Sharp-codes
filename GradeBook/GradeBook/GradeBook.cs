99d7187ce3af0fdde4958113f46aea34c0845968
mohammad hossein keshavarz <mhkb1375@gmail.com>
2022-12-20 10:23:13 +0330

    second part added

---
+++ GradeBook/GradeBook/GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class GradeBook
+    {
+        private String courseName;
+        
+        public GradeBook(String name)
+        {
+            CourseName = name;
+        }
+
+        public GradeBook()
+        {
+
+        }
+        
+        public String CourseName
+        {
+            get
+            {
+                return courseName;
+            }
+            set
+            {
+                courseName = value;
+            }
+        }
+
+        public void DisplayMessage(String nameofCourse)
+        {
+            Console.WriteLine("Welcome to the Grade Book for\n{0}", nameofCourse);
+        }
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Grade Book for\n{0}", CourseName);
+        }
+    }
+}
